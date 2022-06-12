4831d7195ca8a631b7bb69016b3ae0dee3e7f5e1
elisad5791 <elisad5791@mail.ru>
2022-06-12 18:47:44 +0500

    add fuzzy search

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -19,7 +19,7 @@ test('relevance', () => {
   expect(searchEngine.search('shoot')).toEqual(['doc2', 'doc1']);
 });
 
-/*test('fuzzy search', () => {
+test('fuzzy search', () => {
   const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
   const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
   const doc3 = { id: 'doc3', text: "I'm your shooter." };
@@ -27,4 +27,4 @@ test('relevance', () => {
 
   const searchEngine = buildSearchEngine(docs);
   expect(searchEngine.search('shoot at me')).toEqual(['doc2', 'doc1']);
-});*/
+});
--- index.js
+++ index.js
@@ -1,25 +1,35 @@
+const sum = (arr) => arr.reduce((acc, item) => acc + item, 0);
+const getTerm = (word) => word.match(/\w+/g)[0];
+const getWords = (txt) => txt.split(' ').map((item) => getTerm(item));
+const getCount = (word, arr) => arr.reduce((acc, wordArr) => word === wordArr ? acc + 1 : acc, 0);
+const getNotZero = (arr) => arr.reduce((acc, item) => item > 0 ? acc + 1 : acc, 0);
+
 const buildSearchEngine = (docs) => ({
   docs,
-  getTerm(word) {
-    return word.match(/\w+/g)[0];
-  },
-  getWords(txt) {
-    return txt
-      .split(' ')
-      .map((item) => this.getTerm(item));
-  },
-  search(word) {
-    const termWord = this.getTerm(word);
-    return this.docs
-      .map((doc) => {
-        const { id, text } = doc;
-        const docWords = this.getWords(text);
-        const count = docWords.reduce((acc, item) => (item === termWord ? acc + 1 : acc), 0);
-        return { id, count };
-      })
-      .filter((doc) => doc.count > 0)
-      .sort((a, b) => b.count - a.count)
-      .map((doc) => doc.id);
+  search(str) {
+    const strWords = getWords(str);
+    const counters = this.docs.map((doc) => {
+      const { id, text } = doc;
+      const docWords = getWords(text);
+      const counts = strWords
+        .reduce((acc1, strWord) => {
+          const count = getCount(strWord, docWords);
+          acc1.push(count);
+          return acc1;
+        }, []);
+      return { id, counts };
+    });
+    const filteredCounters = counters.filter((counter) => sum(counter.counts) > 0);
+    filteredCounters.sort((a, b) => {
+      const diff = getNotZero(b.counts) - getNotZero(a.counts);
+      if (diff !== 0) {
+        return diff;
+      } else {
+        return sum(b.counts) - sum(a.counts);
+      }
+    });
+    const result = filteredCounters.map((doc) => doc.id);
+    return result;
   },
 });
 
